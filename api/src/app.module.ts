dab74ce23f15f0ab1b0385f078b9af488e4e96c0
anthonymeny <anthony.meny@noctua.care>
2024-12-11 14:14:45 +0100

    fix: basic refacto

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './modules/user/user.module';
-import { MedicationModule } from './modules/medication/medication.module';
-import { AuthentificationModule } from './modules/authentification/authentification.module';
 
 @Module({
   imports: [
-    AuthentificationModule,
     UserModule,
-    MedicationModule,
   ],
   controllers: [],
   providers: [],
