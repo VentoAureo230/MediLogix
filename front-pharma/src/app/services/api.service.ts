a2cd4b664f1e179f82463b0f763fc56886803b38
VentoAureo230 <anthony.meny35@gmail.com>
2025-02-16 18:53:23 +0100

    fix: typo in Angular

--- front-pharma/src/app/services/api.service.ts
+++ front-pharma/src/app/services/api.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
-  private apiUrl = 'http://localhost:3000/authentification';
+  private apiUrl = 'http://localhost:3000/authentication';
 
   constructor(private http: HttpClient) { }
 
